cda9aa5a95c720dee000444ae497840a5556351a
baemingun <baemingun@naver.com>
2016-06-14 11:42:23 +0900

    Add files via upload

--- project.py
+++ project.py
@@ -6,6 +6,47 @@ class Node:
 		self.left = None
 		self.right = None
 
+class Graph:
+  def __init__(self):
+      self.nodes = set()
+      self.edges = defaultdict(list)
+      self.distances = {}
+  def add_node(self, value):
+      self.nodes.add(value)
+  def add_edge(self, from_node, to_node, distance):
+      self.edges[from_node].append(to_node)
+      self.edges[to_node].append(from_node)
+      self.distances[(from_node, to_node)] = distance
+
+def find_min(nodes, visited):
+    min_node = None
+    for node in nodes:
+        if node in visited:
+            if min_node is None:
+                min_node = node
+            elif visited[node] < visited[min_node]:
+                min_node = node
+    return min_node
+
+def relax(graph, visited, current_weight, min_node):
+  for edge in graph.edges[min_node]:
+      weight = current_weight + graph.distance[(min_node, edge)]
+      if edge not in visited or weight < visited[edge]:
+          visited[edge] = weight
+          path[edge] = min_node
+
+def dijsktra(graph, initial):
+    visited = {'initial': 0}
+    path = {}
+    nodes = set(graph.nodes)
+    while nodes:
+        min_node = find_min(nodes)
+        nodes.remove(min_node)
+        current_weight = visited[min_node]
+
+        relax(graph, visited, current_weight, min_node)
+    return visited, path
+
 class RBTNode(Node):
 	def __init__(self):
 		super().__init__()
